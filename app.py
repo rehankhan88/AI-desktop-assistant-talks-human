83720267e471303977751d1e446c4c0e0fa469d8
rehankhan88 <rehan9732088@gmail.com>
2024-08-24 22:46:56 -0700

    I am say function and takeCommand function

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import speech_recognition as sr
+import pyttsx3
 
+def say(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)  # Adjusts for ambient noise
+        r.pause_threshold = 1  # Pause threshold before assuming user is done speaking
+        print("Listening...")
+        try:
+            audio_data = r.listen(source)
+            print("Recognizing...")
+            query = r.recognize_google(audio_data, language="en-PK")
+            print(f"User said: {query}")
+            return query
+        except sr.UnknownValueError:
+            print("Sorry, I did not understand that.")
+            return ""
+        except sr.RequestError as e:
+            print(f"Could not request results; {e}")
+            return ""
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return ""
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print('PyCharm')
+    say("Hello, I am AI")
+    print("Listening...")
+    text = takeCommand()
+    if text:
+        say(text)
